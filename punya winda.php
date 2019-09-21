eda5d13bff10647d93af790409977bc6f4f29d58
windwiendwin <55626429+windwiendwin@users.noreply.github.com>
2019-09-21 20:32:08 +0700

    Add files via upload

---
+++ punya winda.php
@@ -0,0 +1,51 @@
+<?php
+
+
+$a = 9;
+$b = 14;
+$c = 22/7;
+
+
+echo "Rumus Luas Segitiga";
+echo "<br>";
+echo "L = 1/2 x alas x tinggi";
+echo "<br>";
+echo "Contoh =";
+echo "<br>";
+echo "Alas = " . $a;
+echo "<br>";
+echo "Tinggi = " . $b;
+echo "<br>";
+echo "Luas segitiga = " . 1/2*$a*$b ; //
+echo "<br>";
+echo "<br>";
+
+echo "Rumus Luas Lingkaran";
+echo "<br>";
+echo "L = phi x r x r";
+echo "<br>";
+echo "Contoh = ";
+echo "<br>";
+echo "phi = " . $c;
+echo "<br>";
+echo "r = " . $a;
+echo "<br>";
+echo "Luas Lingkaran = " . $c*$a*$a ; 
+echo "<br>";
+echo "<br>";
+
+echo "Rumus Luas Jajar Genjang";
+echo "<br>";
+echo "L = a x t";
+echo "<br>";
+echo "Contoh =";
+echo "<br>";
+echo "Alas = " . $a;
+echo "<br>";
+echo "Tinggi = " . $b;
+echo "<br>";
+echo "Luas Jajar Genjang = " . $a*$b ; 
+echo "<br>";
+echo "<br>";
+
+?>
